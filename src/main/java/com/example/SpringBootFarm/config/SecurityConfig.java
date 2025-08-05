c70089e0ce721c72adc499ff157884746e1b954a
Журкин Максим Юрьевич <ZhurkinMY@it.mos.ru>
2025-08-05 17:36:09 +0500

    Keycloak

--- src/main/java/com/example/SpringBootFarm/config/SecurityConfig.java
+++ src/main/java/com/example/SpringBootFarm/config/SecurityConfig.java
@@ -5,11 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,10 +16,12 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/admin").hasRole("ADMIN")
-                        .requestMatchers("/admin//*").hasRole("ADMIN")
+                        .requestMatchers("/admin").hasAuthority("ROLE_farm.admin")
+                        .requestMatchers("/admin//*").hasAuthority("ROLE_farm.admin")
                         .anyRequest().authenticated()
                 )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())))
                 .formLogin(
                         login -> login
                                 .defaultSuccessUrl("/farm", true)
@@ -32,17 +30,9 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-       UserDetails user = User.withUsername("user")
-               .password(passwordEncoder().encode("Spring@123")).roles("USER").build();
-       UserDetails admin = User.withUsername("admin")
-               .password(passwordEncoder().encode("Spring@321")).roles("ADMIN").build();
-       return new InMemoryUserDetailsManager(user, admin);
-    }
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        var converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(new KeycloakJwtAuthenticationConverter());
+        return converter;
     }
 }
